8117ce4e2d81c976d5c4385d17c79a73e3fa6fab
xiaofeige <1149151981@qq.com>
2017-03-16 18:32:01 +0800

    创建新的项目

---
+++ ActionAnalyze.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: luffyren
+@site: http://www.luffyren.club
+@software: PyCharm Community Edition
+@file: ActionAnalyze.py
+@time: 2017/3/16 14:14
+"""
+
+import sys
+
+
+def show_how_to_use():
+    print u"""
+    --file, 指定需要分析的文件
+    """
+
+
+def main():
+    """
+    行为分析脚本入口
+    """
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+
