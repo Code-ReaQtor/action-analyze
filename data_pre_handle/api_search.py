3854adc6cc8184b891109228a642acb24cfc2257
xiaofeige <1149151981@qq.com>
2017-03-20 11:27:12 +0800

    修复了csv文件过大导致越界的bug

--- data_pre_handle/api_search.py
+++ data_pre_handle/api_search.py
@@ -13,6 +13,7 @@
 
 import json
 import csv
+import sys
 
 
 def generate_api_map(filename):
@@ -20,11 +21,16 @@ def generate_api_map(filename):
     生成API 编号表
     :return:
     """
+    csv.field_size_limit(sys.maxsize)
+    
     api_map = {}
     file_in = file(filename, 'rb')
     reader = csv.reader(file_in)
-    for line in reader:
-        api_map[line[28]] = line[49]
+    try:
+        for line in reader:
+            api_map[line[28]] = line[49]
+    except csv.Error:
+        print "文件长度超过限制。系统不好解决。"
 
     file_out = open('../data/api_map.json', 'w')
     # 将得到的dist 按照 value 值进行排序 得到一个固定的顺序
