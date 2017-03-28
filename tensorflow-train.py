b82f3ecd9a25dcad1a59af7f79a1b9e69df42ed6
xiaofeige <1149151981@qq.com>
2017-03-28 21:44:40 +0800

    添加了tensorflow测试代码

--- tensorflow-train.py
+++ tensorflow-train.py
@@ -9,8 +9,18 @@ import tensorflow as tf
 import sys
 
 
+def test_tf():
+    a = tf.placeholder("float")
+    b = tf.placeholder("float")
+    y = tf.multiply(a, b)
+    sess = tf.Session()
+    print sess.run(y, feed_dict={a: 3, b: 3})
+
+
+
 def main():
     print "tensorflow surport added"
+    test_tf()
     return 0
 
 
