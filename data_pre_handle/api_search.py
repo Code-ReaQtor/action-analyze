99d955f40b516fb580e60b1bc857e25e8c103c9b
xiaofeige <1149151981@qq.com>
2017-03-29 20:12:28 +0800

    添加了普通文件读写方式下的读取，尝试解决大文件去屑错误问你题

--- data_pre_handle/api_search.py
+++ data_pre_handle/api_search.py
@@ -41,6 +41,29 @@ def generate_api_map(filename):
     file_out.close()
 
 
+def generate_api_map_t(filename):
+    """
+    通过传统的读文件方式来读取大文件，试着解决之前文件太大导致的问题
+    :return:
+    """
+    api_map = {}
+    file_in = open(filename, 'rb')
+    try:
+        for line in file_in:
+            segments = line.split(',')
+            api_map[segments[28]] = segments[49]
+    except csv.Error:
+        print "文件长度超过限制。系统不好解决。"
+
+    file_out = open('../data/api_map.json', 'w')
+    # 将得到的dist 按照 value 值进行排序 得到一个固定的顺序
+    api_map_data = json.dumps(api_map)
+    file_out.write(api_map_data)
+
+    file_in.close()
+    file_out.close()
+
+
 if __name__ == '__main__':
     generate_api_map(filename="../data/683.csv")
 
