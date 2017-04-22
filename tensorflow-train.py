83fd6e1d25b37417668eb44bb00b859dbb701e98
xiaofeige <1149151981@qq.com>
2017-04-22 15:35:38 +0800

    fix dimesion info

--- tensorflow-train.py
+++ tensorflow-train.py
@@ -50,7 +50,7 @@ def tf_train():
     tf_vecs = tf.constant(vectors)
     # 当前还不清楚如何取Ｋ值，这里首先就用４
     k = 4
-    centroides = tf.Variable(tf.slice(tf.random_shuffle(vectors), [0, 0], [k, -1]))
+    centroides = tf.Variable(tf.slice(tf.random_shuffle(vectors), [0, 0], [k, 17]))
     expand_vecs = tf.expand_dims(tf_vecs, 0)
     expand_cens = tf.expand_dims(centroides, 1)
     diff = tf.sub(expand_vecs, expand_cens)
