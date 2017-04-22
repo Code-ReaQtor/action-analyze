26b9a937a373cc05521fd33fffa70a7d68d1ef5c
xiaofeige <1149151981@qq.com>
2017-04-22 21:50:45 +0800

    kmean function

--- tensorflow-train.py
+++ tensorflow-train.py
@@ -60,7 +60,12 @@ def tf_train():
 
     #计算图心
     print "compute centroids"
-    means = tf.concat(0, [tf.reduce_mean(tf.gather(vectors, tf.reshape(tf.where(tf.equal(assignments, [1])), [1, -1])), reduction_indices=[1])])
+    # means = tf.concat(0, [tf.reduce_mean(tf.gather(vectors, tf.reshape(tf.where(tf.equal(assignments, c)), [1, -1])), reduction_indices=[1])for c in xrange(k)])
+
+    reshape_result = tf.reshape(tf.where(tf.equal(assignments, [1]), [1, -1]))
+    gather_result = tf.gather(vectors, reshape_result)
+    reduce_mean = tf.reduce_mean(gather_result, reduction_indices=[1])
+    means = tf.concat(0, [reduce_mean])
     update_centroides = tf.assign(centroides, means)
     init_op = tf.initialize_all_variables()
 
