d636ca1cd0bd537b0ec011e7f9a1bd0c336c3cb6
xiaofeige <1149151981@qq.com>
2017-04-26 04:56:55 +0800

    add word2vec surport

---
+++ data_pre_handle/w2v.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author:luffyren-ubuntu-vm
+@date:
+@DESC:本脚本将目标文件夹中的所有行为api 提取变成一个个的分词形式
+"""
+
+import csv
+import json
+import sys
+
+
+def w2c():
+    pass
+
+
+def main():
+    w2c()
+    return 0
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
