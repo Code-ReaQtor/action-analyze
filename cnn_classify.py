d70ea15793ab723e6a1f2a7ec8270a73153fc091
xiaofeige <1149151981@qq.com>
2017-04-27 20:01:34 +0800

    add cnn classify implement

---
+++ cnn_classify.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: luffyren
+@site: http://www.luffyren.club
+@software: PyCharm Community Edition
+@file: cnn_classify.py
+@time: 2017/4/27 20:00
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
