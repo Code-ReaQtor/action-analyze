3243de582bed1ef6be1a526aabd3ec29e68c4d70
xiaofeige <1149151981@qq.com>
2017-05-03 03:53:18 +0800

    add softmax classify

---
+++ softmax_classify.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author:luffyren-ubuntu-vm
+@date:
+@DESC:
+"""
+
+import tensorflow as tf
+
+
+def softmax():
+    pass
+
+
+if __name__ == '__main__':
+    softmax()
