4d9b4b907cc777ad79822fb85d629954e69f88ae
xiaofeige <1149151981@qq.com>
2017-05-04 19:27:03 +0800

    add cnn function

--- cnn_classify.py
+++ cnn_classify.py
@@ -16,6 +16,16 @@ import tensorflow as tf
 import json
 
 
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+
 def conv2d(x, W):
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
@@ -25,6 +35,23 @@ def max_pool_2x2(x):
 
 
 def main():
+    """
+    
+    :return: 
+    """
+    #
+    W_conv1 = weight_variable([5, 5, 1, 32])
+    b_conv1 = bias_variable([32])
+
+    # conv layer-1
+    x = tf.placeholder(tf.float32, [None, 784])
+    x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+    h_pool1 = max_pool_2x2(h_conv1)
+
+    
+
     return 0
 
 
