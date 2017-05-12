265a7960ff900321aa8839f6bed174713927a9f6
xiaofeige <1149151981@qq.com>
2017-05-12 16:00:18 +0800

    add some space

--- softmax_classify.py
+++ softmax_classify.py
@@ -21,15 +21,18 @@ def get_data():
 def soft_max():
     x = tf.placeholder(tf.float32, [None, 61])
 
-    # 程序的分类现在先不能确定，今天晚上大概还需要在确定下,先以10类为准
-    W = tf.Variable(tf.zeros([784, 10]))
-    b = tf.Variable(tf.zeros(10))
+    # 程序的分类现在先不能确定，今天晚上大概还需要在确定下,先以7类为准
+
+    W = tf.Variable(tf.zeros([61, 7]))
+    b = tf.Variable(tf.zeros(7))
 
     y = tf.nn.softmax(tf.matmul(x, W) + b)
 
     #损失函数
-    y_ = tf.placeholder(tf.float32, [None, 10])
+
+    y_ = tf.placeholder(tf.float32, [None, 7])
     cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
+
     train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
     #初始化
